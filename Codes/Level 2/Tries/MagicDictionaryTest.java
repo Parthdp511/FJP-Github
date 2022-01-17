389c82a3b277a3a9f89d4bfc11edc5afd25cfd18
Parthdp511 <parth@factors.ai>
2022-01-17 08:54:28 +0530

    Changed Magic Dictionary Code

---
+++ Codes/Level 2/Tries/MagicDictionaryTest.java
@@ -0,0 +1,84 @@
+class MagicDictionary {
+    public static class Node {
+        private Node[] children = new Node[26];
+        private boolean isTerminal = false;
+
+        public Node get(char ch) {
+            return children[ch - 'a'];
+        }
+
+        public boolean isTerminal() {
+            return isTerminal;
+        }
+
+        public void setTerminal() {
+            isTerminal = true;
+        }
+
+        public void add(char ch) {
+            children[ch - 'a'] = new Node();
+        }
+
+        public boolean contains(char ch) {
+            return (children[ch - 'a'] != null);
+        }
+    }
+
+    Node root;
+
+    public MagicDictionary() {
+        root = new Node();
+    }
+
+    public void buildDict(String[] dictionary) {
+        for (String word : dictionary) {
+            Node curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+
+                if (curr.contains(ch) == false)
+                    curr.add(ch);
+
+                curr = curr.get(ch);
+            }
+            curr.setTerminal();
+        }
+    }
+
+    public boolean search(String word, int idx, Node curr, boolean change) {
+        if (idx == word.length()) {
+            System.out.println(change + " wtf " + curr.isTerminal());
+            if (change == true && curr.isTerminal() == true)
+                return true;
+            return false;
+        }
+
+        char ch = word.charAt(idx);
+        // if the word exist in the trie
+        if (curr.contains(ch) == true && search(word, idx + 1, curr.get(ch), change)) {
+            return true;
+        }
+
+        
+            // if it doesn't exist replace the wore except itself
+            if (change == true)
+                return false;
+            for (char chn = 'a'; chn <= 'z'; chn++) {
+                if (chn == ch)
+                    continue;
+                // if the current word exist in the trie,
+                // mark the word as replaced and furtur check aage trie mai
+                // aaga chlna sa kaam hojaye toh
+                if (curr.contains(chn) && search(word, idx + 1, curr.get(chn), true)) {
+                    return true;
+                }
+            }
+        
+
+        return false;
+    }
+
+    public boolean search(String searchWord) {
+        return search(searchWord, 0, root, false);
+    }
+}
